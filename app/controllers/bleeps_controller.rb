14eeee780f6d0f6cc24cae3b473023d25798c7fe
Shahwaiz Qureshi <shay.que.official@gmail.com>
2020-06-24 13:14:33 +1000

    basic bootstrap working

--- app/controllers/bleeps_controller.rb
+++ app/controllers/bleeps_controller.rb
@@ -7,9 +7,21 @@ class BleepsController < ApplicationController
     @bleep = Bleep.new
   end
 
+  def create
+    bleep = Bleep.create bleep_params
+    @current_user.bleeps << bleep
+    redirect_to root_path
+  end
+
   def edit
   end
 
   def show
   end
+
+  private
+  def bleep_params
+    params.require(:bleep).permit(:content)
+  end
+
 end
